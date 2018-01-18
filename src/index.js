c3f79666dd0a20f595a0cbb31f0ff999682c7ef7
Jeremy Greer <jeremy.greer.atl@gmail.com>
2018-01-17 21:25:39 -0600

    refactor

--- src/index.js
+++ src/index.js
@@ -22,12 +22,10 @@ const updateByIndex = (list, index, newItem) => {
   return replaceByIndex(list, index, mergedItem)
 }
 
-// Finds index if id is a ref to an item or if it is
-// an id value used within one of the list items.
-export const getIndex = (list, needle) => {
+const getIndex = (list, needle) => {
   const index = list.indexOf(needle)
   return index === -1
-    ? list.findIndex(item => item.id === needle || item.id === needle.id)
+    ? list.findIndex(item => item.id === needle.id)
     : index
 }
 
@@ -38,33 +36,31 @@ export const remove = (list, itemOrId) => {
     : list
 }
 
-export const replace = (list, itemOrId, item) => {
-  if (!itemOrId) {
-    throw new Error('You are missing the itemOrId in your call to `replace`.')
+export const replace = (list, item) => {
+  if (!item) {
+    throw new Error('You are missing the item in your call to `replace`.')
   }
-  const id = item ? itemOrId : itemOrId.id
-  const index = getIndex(list, id)
+  const index = getIndex(list, item)
   return index > -1
-    ? replaceByIndex(list, index, item || itemOrId)
+    ? replaceByIndex(list, index, item)
     : (() => {
-      throw new Error(`item with id "${id}" not found in list`)
+      throw new Error(`item with id "${item.id}" not found in list`)
     })()
 }
 
-export const update = (list, item, newItem) => {
+export const update = (list, item) => {
   if (!item) {
     throw new Error('You are missing the item in your call to `update`.')
   }
   const index = getIndex(list, item)
   return index > -1
-    ? updateByIndex(list, index, newItem || item)
+    ? updateByIndex(list, index, item)
     : (() => {
       throw new Error(`item with id "${item.id}" not found in list`)
     })()
 }
 
 export default {
-  getIndex,
   remove,
   replace,
   update,
